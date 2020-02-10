c5acc6c3347b1902affde808ab84bdc3afaeb5fc
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-10 13:39:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,6 +10,15 @@ class CashRegister
   def add_item(title,price,quantity = 1)
     @total = @total + (price * quantity)
     @total
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
     end
   def apply_discount
     if @discount > 0

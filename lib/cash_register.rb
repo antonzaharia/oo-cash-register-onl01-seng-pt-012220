a66d6a2fc52feb0fd0e02c2172559f077ee336ff
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-10 13:36:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,11 @@
 class CashRegister
-  attr_accessor :total
-  
-  def initialize(total = 0)
-    @total = total
+  class CashRegister
+  attr_accessor :total, :discount, :price, :items
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
   end
   def discount(discount = 20)
     discount
